a895d006f2c53af90cad57c9551c1628c57ef57a
marcin walendzik <marcin.walendzik@gmail.com>
2014-12-09 18:42:22 +0000

    transfering the bikes

--- lib/van.rb
+++ lib/van.rb
@@ -3,11 +3,30 @@ class Van
 VAN_CAPACITY = 5
 
 	def initialize(options = {})
-		@van = options.fetch(:capcaity, VAN_CAPACITY)
+		@capacity = options.fetch(:capcaity, VAN_CAPACITY)
 		@bikes = []
 	end
 
 	def bikes_in_van
 		@bikes.count
 	end
+
+	def free_space
+		@capacity - bikes_in_van
+	end	
+
+	def load(bike)
+		@bikes << bike
+	end
+    
+	def collect(station)
+		bikes_to_be_collected = station.broken_bikes.count
+		to_take = [free_space,bikes_to_be_collected].min
+		n = 0
+		while n < to_take
+			bike_to_take = station.broken_bikes[n]
+			load(station.release(bike_to_take))
+			n += 1
+		end
+	end	
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,13 +1,28 @@
 require './lib/van'
 
+
 describe Van do 
 
-let(:van) {Van.new}	
-let(:station) {Station.new}
-let(:bike) {Bike.new}
+  let(:van) {Van.new}	
+  let(:station) {DockingStation.new}
+  let(:bike) {Bike.new}
 
-it "should have no bikes initially" do
-	expect(van.bikes_in_van).to eq (0)
-end
-	
-end
+  it "should have no bikes initially" do
+  	expect(van.bikes_in_van).to eq (0)
+  end
+
+  it "should take a bike" do
+  	van.load(bike)
+  	expect(van.bikes_in_van).to eq(1)
+  end
+
+  it "should collect broken bikes from the station" do
+	bkn_bike,wking_bike = Bike.new, Bike.new
+	bkn_bike.break!
+	station.dock(wking_bike)
+	station.dock(bkn_bike)
+	van.collect(station)
+	expect(van.bikes_in_van).to eq(1)
+	expect(station.broken_bikes.count).to eq(0)
+  end
+end  
